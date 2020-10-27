ae807f1ce428fd60b97ff532c8f13aa07a3daec1
immalz <alzamora1008@gmail.com>
2020-10-27 15:40:38 -0500

    seccion de educacion añadido

--- js/main.js
+++ js/main.js
@@ -38,3 +38,32 @@ $(document).ready(function() {
         $('.menu-btn i').toggleClass("active");
     });
 });
+
+
+$(document).ready(function() {
+    var galleryThumbs = new Swiper(".gallery-thumbs", {
+        direction: "horizontal",
+        spaceBetween: 10,
+        slidesPerView: 3,
+        freeMode: false,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        breakpoints: {
+            480: {
+                direction: "vertical",
+                slidesPerView: 3,
+            }
+        }
+    });
+    var galleryTop = new Swiper(".gallery-top", {
+        direction: "horizontal",
+        spaceBetween: 10,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        },
+        thumbs: {
+            swiper: galleryThumbs
+        }
+    });
+});
