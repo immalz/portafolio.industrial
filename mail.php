5d202d55a6598df05d0a15437321f00376c268ac
immalz <alzamora1008@gmail.com>
2020-10-29 09:56:40 -0500

    añadiendo los videos

--- mail.php
+++ mail.php
@@ -5,22 +5,22 @@
     $subject = $_POST['asunto'];
     $message = $_POST['mensaje'];
 
-    $header = 'From: ' . $mail . "\r\n";
+    $header = 'From: ' . $email . "\r\n";
     $header = "X-Mailer: PHP/" . phpversion() . "\r\n";
     $header = "Mime-Version: 1.0 \r\n";
     $header = "Content-Type: text/plain";
 
 
     $message = "Este mensaje fue enviado por: " .$name . "\r\n";
-    $message .="El correo es: " . $mail . "\r\n";
-    $message .= "Mensaje: " . $_POST['message'] . "\r\n";
+    $message .="El correo es: " . $email . "\r\n";
+    $message .= "Mensaje: " . $_POST['mensaje'] . "\r\n";
     $message .= "Enviado el: " . date('d/m/Y' , time());
 
     $para = 'tcode2319@gmail.com';
     
-    mail($para, $subject, utf8_decode($message, $header));
+    mail($para, $subject, utf8_decode($message), $header);
 
-    header("Location:index.html");
+    header("Location:index.html"); 
 ?>
 
 
